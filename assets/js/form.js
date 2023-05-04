616d8d481cef10cce8f0e7da17d97fb02cce06ac
jlm0 <jordymcnab@kryptic.io>
2023-05-04 13:35:54 -0500

    Update Handle Submit

--- assets/js/form.js
+++ assets/js/form.js
@@ -1,10 +1,22 @@
 function submitForm(event) {
   event.preventDefault(); // Prevent the default form submission behavior
 
-  const myForm = event.target;
-  const formData = new FormData(myForm);
+  // Get the form element
+  const form = event.target;
+
+  // Check if reCAPTCHA is filled
+  const recaptchaResponse = grecaptcha.getResponse();
+  if (recaptchaResponse.length === 0) {
+    // Show error message below the input
+    const errorMessage = document.createElement("p");
+    errorMessage.textContent = "Please complete the reCAPTCHA.";
+    errorMessage.style.color = "red";
+    form.appendChild(errorMessage);
+    return;
+  }
 
   // Send the form data to Netlify
+  const formData = new FormData(form);
   fetch("/", {
     method: "POST",
     headers: { "Content-Type": "application/x-www-form-urlencoded" },
@@ -12,8 +24,15 @@ function submitForm(event) {
   })
     .then((response) => {
       if (response.ok) {
-        // Show success message
-        alert("Form submitted successfully!");
+        // Clear the form
+        form.reset();
+        grecaptcha.reset();
+
+        // Show success message below the input
+        const successMessage = document.createElement("p");
+        successMessage.textContent = "Thank you! Form submitted successfully!";
+        successMessage.style.color = "green";
+        form.appendChild(successMessage);
       } else {
         // Show error message
         alert("There was an error submitting the form. Please try again.");
